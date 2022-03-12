ed7f7cc0756d4ab640af8aa48f180ae0507ded42
Alexey Ivannikov <alexey.a.ivannikov@gmail.com>
2022-03-12 23:03:47 +0300

    file-server-get implemented

--- 03-streams/03-file-server-get/server.js
+++ 03-streams/03-file-server-get/server.js
@@ -1,6 +1,7 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
@@ -8,10 +9,31 @@ server.on('request', (req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`);
   const pathname = url.pathname.slice(1);
 
+  if (pathname.includes('/')) {
+    res.statusCode = 400;
+    res.end('Wrong request');
+    return;
+  }
+
   const filepath = path.join(__dirname, 'files', pathname);
 
   switch (req.method) {
     case 'GET':
+      const stream = fs.createReadStream(filepath);
+
+      res.on('aborted', () => { stream.destroy(); });
+
+      stream.on('error', error => {
+        if (error.code === 'ENOENT') {
+          res.statusCode = 404;
+          res.end('File not found');
+        } else {
+          res.statusCode = 500;
+          res.end('Internal server error');
+        }
+      });
+
+      stream.pipe(res);
 
       break;
 
