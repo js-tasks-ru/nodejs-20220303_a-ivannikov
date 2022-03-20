698a774e8147ddc8cfe8bebbc0008a7cc180c030
Alexey Ivannikov <alexey.a.ivannikov@gmail.com>
2022-03-20 23:13:08 +0300

    Chat application implemented

--- 05-koajs/01-chat-app/app.js
+++ 05-koajs/01-chat-app/app.js
@@ -8,10 +8,36 @@ app.use(require('koa-bodyparser')());
 const Router = require('koa-router');
 const router = new Router();
 
+const waiters = new Map();
+
 router.get('/subscribe', async (ctx, next) => {
+    const id = `${ctx.socket.remoteAddress}:${ctx.socket.remotePort}`;
+
+    const data = await new Promise(resolve => {
+        waiters.set(id, resolve);
+    });
+
+    if (data.message) {
+        ctx.body = data.message;
+    } else {
+        ctx.status = 204;
+    }
+
+    waiters.delete(id);
 });
 
+const isMessageNotEmpty = (ctx) => ctx.request.body.message?.length > 0;
+
 router.post('/publish', async (ctx, next) => {
+    if (isMessageNotEmpty(ctx)) {
+        for (const resolve of waiters.values()) {
+            resolve(ctx.request.body);
+        }
+
+        ctx.body = 'Message sent';
+    } else {
+        ctx.body = 'Empty message ignored';
+    }
 });
 
 app.use(router.routes());
