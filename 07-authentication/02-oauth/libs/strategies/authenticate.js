0f5ee8102532e6ff5ad2f2be425e5714b659c898
Alexey Ivannikov <alexey.a.ivannikov@gmail.com>
2022-04-03 20:56:32 +0300

    Решена задача 7/2

--- 07-authentication/02-oauth/libs/strategies/authenticate.js
+++ 07-authentication/02-oauth/libs/strategies/authenticate.js
@@ -1,3 +1,20 @@
-module.exports = function authenticate(strategy, email, displayName, done) {
-  done(null, false, `функция аутентификации с помощью ${strategy} не настроена`);
-};
+const User = require('../../models/User');
+
+module.exports = async function authenticate(strategy, email, displayName, done) {
+  if (!email) {
+    done(null, false, 'Не указан email');
+    return;
+  }
+
+  try {
+    let user = await User.findOne({ email: email });
+
+    if (!user) {
+      user = await User.create({ email: email, displayName: displayName });
+    }
+
+    done(null, user);
+  } catch (error) {
+    done(error);
+  }
+}
